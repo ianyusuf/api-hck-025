1f922a7e86179003a0ae0353727dcb45c4738a2a
Rania Alyandra <hadyanyusuf@gmail.com>
2025-02-04 15:48:57 +0700

    add tambah data

--- main.py
+++ main.py
@@ -35,13 +35,11 @@ def search_data(id:int):
 # http://127.0.0.1:8000/data
 
 # Menambahkan data
-# @app.post("/data/{new_data}")
-# def add_data(data:dict):
-#     new_data = new_data.string('-')
-#     new_row = {'id':new_data[0],
-#         'nama':new_data[1],
-#         'age':new_data[2],
-#         'job':new_data[3]}
-#     new_row = pd.DataFrame([new_row])
-#     data = pd.concat([data, new_row],ignore_index=True)
-#     return {'message':'data id updated !'}
+@app.post("/data/add")
+def add_data(new_data:dict):
+    global data
+    
+    new_row = pd.DataFrame([new_data])
+    data = pd.concat([data, new_row], ignore_index=True)
+
+    return {'message':data.to_dict(orient='records')}
